846b25eb64f3c167a5ae431be6cb8a4ef1e56f27
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2019-12-12 14:59:23 +0000

    [ Componente 'spinner' ] » Nuevo componente creado e integrado en la página 'login'

---
+++ src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent {
+
+  @Input() showSpinner = false;
+
+  constructor() { }
+
+}
